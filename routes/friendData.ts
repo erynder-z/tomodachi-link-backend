f2167f0935601e0739150b05112165e169a0ebdd
erynder-z <stfn.bgr@gmail.com>
2023-04-19 22:22:54 +0200

    Fix typo

--- routes/friendData.ts
+++ routes/friendData.ts
@@ -5,7 +5,7 @@ import * as friendDataController from '../controllers/friendDataController';
 export const friendDataRoute = Router();
 
 friendDataRoute.get(
-    '/friendData',
+    '/frienddata',
     passport.authenticate('jwt', { session: false }),
     friendDataController.getFriendData
 );
