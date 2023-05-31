1b947566d6412b5efaeb5078a86b1ac2b0ec1f09
erynder-z <stfn.bgr@gmail.com>
2023-05-31 21:49:45 +0200

    Update types

--- types/postDocument.ts
+++ types/postDocument.ts
@@ -3,4 +3,5 @@ import mongoose from 'mongoose';
 export type PostDocument = mongoose.Document<any, any, any> & {
     _id: mongoose.Types.ObjectId;
     timestamp: Date;
+    owner: { _id: string };
 };
