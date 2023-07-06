e9c082449e73217248e6510497f9bd59986d448b
erynder-z <stfn.bgr@gmail.com>
2023-07-06 21:23:55 +0200

    Refactor socket handling

--- socket/initializeSocketIo.ts
+++ socket/initializeSocketIo.ts
@@ -1,6 +1,5 @@
 import { Server } from 'socket.io';
 import http from 'http';
-
 export const initializeSocketIo = (
     server: http.Server,
     corsOptions: {
@@ -11,19 +10,46 @@ export const initializeSocketIo = (
         cors: corsOptions,
     });
 
+    let users: { userId: string; socketId?: string }[] = [];
+
+    const addUser = (userId: string, socketId: string) => {
+        !users.some((user) => user.userId === userId) &&
+            users.push({ userId, socketId });
+    };
+
+    const removeUser = (socketId: string) => {
+        users = users.filter((user) => user.socketId !== socketId);
+    };
+
+    const getUser = (userId: string) => {
+        return users.find((user) => user.userId === userId);
+    };
+
     io.on('connection', (socket) => {
         console.log(`user connected: ${socket.id}`);
 
-        socket.on('joinRoom', (data) => {
-            socket.join(data);
+        socket.on('addUser', (userId) => {
+            addUser(userId, socket.id);
+            io.emit('getUsers', users);
         });
 
-        socket.on('sendMessage', (data) => {
-            socket.to(data.chatroomId).emit('receiveMessage', data);
-        });
+        socket.on(
+            'sendMessage',
+            ({ senderId, receiverId, text, timestamp }) => {
+                console.log({ senderId, receiverId, text });
+                const user = getUser(receiverId);
+                io.to(user?.socketId as string).emit('receiveMessage', {
+                    senderId,
+                    text,
+                    timestamp,
+                });
+            }
+        );
 
         socket.on('disconnect', () => {
             console.log(`User disconnected: ${socket.id}`);
+            removeUser(socket.id);
+            io.emit('getUsers', users);
         });
     });
 };
