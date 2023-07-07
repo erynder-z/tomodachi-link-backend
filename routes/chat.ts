2fc7d295de436890fdedcbac7d7189ae49d2c5da
erynder-z <stfn.bgr@gmail.com>
2023-07-07 20:31:18 +0200

    add chat routes

--- routes/chat.ts
+++ routes/chat.ts
@@ -4,8 +4,38 @@ import * as chatController from '../controllers/chatController';
 
 export const chatRoute = Router();
 
-chatRoute.get(
+/* chatRoute.get(
     '/users/:id/chatroom',
     passport.authenticate('jwt', { session: false }),
     chatController.getChatroomId
+); */
+
+chatRoute.post(
+    '/chat',
+    passport.authenticate('jwt', { session: false }),
+    chatController.initializeConversation
+);
+
+chatRoute.get(
+    '/chat/:userId',
+    passport.authenticate('jwt', { session: false }),
+    chatController.getConversationOfSingleUser
+);
+
+chatRoute.get(
+    '/chat/:userId1/:userId2',
+    passport.authenticate('jwt', { session: false }),
+    chatController.getConversationBetweenTwoUsers
+);
+
+chatRoute.post(
+    '/message',
+    passport.authenticate('jwt', { session: false }),
+    chatController.addChatMessage
+);
+
+chatRoute.get(
+    '/message/:messageId',
+    passport.authenticate('jwt', { session: false }),
+    chatController.getChatMessage
 );
