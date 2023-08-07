b8f00d09ff114b03e612a9b2de0300842d5f590a
erynder-z <stfn.bgr@gmail.com>
2023-08-07 19:56:57 +0200

    Prevent access to chat for guest-account

--- routes/chat.ts
+++ routes/chat.ts
@@ -1,41 +1,48 @@
 import { Router } from 'express';
 import passport from 'passport';
 import * as chatController from '../controllers/chatController';
+import { checkAccountType } from '../middleware/checkAccountType';
 
 export const chatRoute = Router();
 
 chatRoute.post(
     '/chat',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.initializeConversation
 );
 
 chatRoute.get(
     '/chat',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.getConversationOfUser
 );
 
 chatRoute.post(
     '/message',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.addChatMessage
 );
 
 chatRoute.get(
     '/message/:conversationId',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.getMessagesFromConversation
 );
 
 chatRoute.patch(
     '/message/:conversationId/unread',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.markConversationAsUnread
 );
 
 chatRoute.patch(
     '/message/:conversationId/read',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     chatController.markConversationAsRead
 );
