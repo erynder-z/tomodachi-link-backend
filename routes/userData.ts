930ac20d1e5ce1d24f8daa0698c357a254a0cb8c
erynder-z <stfn.bgr@gmail.com>
2023-08-08 19:54:56 +0200

    Validate account type for userdata updates

--- routes/userData.ts
+++ routes/userData.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import passport from 'passport';
 import * as userDataController from '../controllers/userDataController';
 import multer from 'multer';
+import { checkAccountType } from '../middleware/checkAccountType';
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage, limits: { fileSize: 1000000 } }); // max 1 mb
@@ -18,6 +19,7 @@ userDataRoute.put(
     '/userdata',
     passport.authenticate('jwt', { session: false }),
     upload.single('imagePicker'),
+    checkAccountType,
     userDataController.updateUserData
 );
 
@@ -30,5 +32,6 @@ userDataRoute.patch(
 userDataRoute.patch(
     '/password',
     passport.authenticate('jwt', { session: false }),
+    checkAccountType,
     userDataController.updateUserPassword
 );
