92ef5b498e30401d62319b7bae774f43704966a3
erynder-z <stfn.bgr@gmail.com>
2023-11-12 17:59:51 +0100

    Refactor comment creation

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -9,70 +9,82 @@ import {
 } from 'obscenity';
 import mongoose from 'mongoose';
 
-const createComment = [
+const validateCreateComment = [
     body('newComment', 'Text must not be empty.')
         .trim()
         .isLength({ min: 1 })
         .escape(),
-
-    async (req: Request, res: Response, next: NextFunction) => {
-        const errors = validationResult(req);
-
-        const { id } = req.params;
-        const { newComment } = req.body;
-        const { user } = req;
-
-        try {
-            const ERROR_MESSAGE =
-                'Invalid parentItem ID. It must be a valid Post or Poll ID.';
-            const SUCCESS_MESSAGE = 'Comment created successfully!';
-
-            const parentItem =
-                (await mongoose.model('Post').findById(id).exec()) ||
-                (await mongoose.model('Poll').findById(id).exec());
-
-            if (!parentItem) {
-                return res.status(400).json({
-                    error: ERROR_MESSAGE,
-                });
-            }
-
-            const matcher = new RegExpMatcher({
-                ...englishDataset.build(),
-                ...englishRecommendedTransformers,
-            });
-
-            const censor = new TextCensor();
-            const matches = matcher.getAllMatches(newComment);
-
-            const censoredText = censor.applyTo(newComment, matches);
-
-            const comment = new Comment({
-                parentItem: id,
-                owner: user,
-                text: censoredText,
-            });
-
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    errors: errors.array(),
-                });
-            }
-
-            const savedComment = await comment.save();
-
-            await parentItem.updateOne({
-                $push: { comments: savedComment._id },
-            });
-
-            res.status(200).json({
-                title: SUCCESS_MESSAGE,
-                savedComment,
-            });
-        } catch (err) {
-            return next(err);
-        }
-    },
 ];
+
+const createCommentHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const errors = validationResult(req);
+    const { id } = req.params;
+    const { newComment } = req.body;
+    const { user } = req;
+
+    const ERROR_MESSAGE =
+        'Invalid parentItem ID. It must be a valid Post or Poll ID.';
+    const SUCCESS_MESSAGE = 'Comment created successfully!';
+
+    try {
+        const parentItem = await findParentItem(id);
+
+        if (!parentItem) {
+            return res.status(400).json({ error: ERROR_MESSAGE });
+        }
+
+        const censoredText = censorText(newComment);
+
+        const comment = new Comment({
+            parentItem: id,
+            owner: user,
+            text: censoredText,
+        });
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const savedComment = await comment.save();
+
+        await parentItem.updateOne({
+            $push: { comments: savedComment._id },
+        });
+
+        res.status(200).json({
+            title: SUCCESS_MESSAGE,
+            savedComment,
+        });
+    } catch (err) {
+        return next(err);
+    }
+};
+
+const findParentItem = async (id: string) => {
+    const [Post, Poll] = await Promise.all([
+        mongoose.model('Post').findById(id).exec(),
+        mongoose.model('Poll').findById(id).exec(),
+    ]);
+
+    return Post || Poll;
+};
+
+const censorText = (text: string) => {
+    const matcher = new RegExpMatcher({
+        ...englishDataset.build(),
+        ...englishRecommendedTransformers,
+    });
+
+    const censor = new TextCensor();
+    const matches = matcher.getAllMatches(text);
+
+    return censor.applyTo(text, matches);
+};
+
+const createComment = [...validateCreateComment, createCommentHandler];
 
 export { createComment };
