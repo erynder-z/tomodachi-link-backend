aea275a695f94eb9e335a111483da3830dd20c94
erynder-z <stfn.bgr@gmail.com>
2023-11-13 18:35:21 +0100

    Fix possible null pointer

--- controllers/getApiKeysController.ts
+++ controllers/getApiKeysController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const getTenorKey = (req: Request, res: Response) => {
-    const key = process.env.TENOR_API_KEY;
+    const key = process.env.TENOR_API_KEY || '';
     res.json({ key });
 };
 
