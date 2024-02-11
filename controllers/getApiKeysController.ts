4b7216e108a2675e8c7f4813877ba1fc5473a9dc
erynder-z <stfn.bgr@gmail.com>
2024-02-11 14:05:12 +0100

    Add documentation for api key controller

--- controllers/getApiKeysController.ts
+++ controllers/getApiKeysController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
 
-const getTenorKey = (req: Request, res: Response) => {
+/**
+ * Retrieves the Tenor API key from the environment variables and sends it as a JSON response.
+ *
+ * @param {Request} req - the request object
+ * @param {Response} res - the response object
+ * @return {void} JSON response containing the Tenor API key
+ */
+const getTenorKey = (req: Request, res: Response): void => {
     const key = process.env.TENOR_API_KEY || '';
     res.json({ key });
 };
