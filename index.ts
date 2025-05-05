bd8e38b6cab7fef55028058c50213dc31d0fb578
erynder-z <stfn.bgr@gmail.com>
2025-05-05 23:14:22 +0200

    Update cors handling

--- index.ts
+++ index.ts
@@ -22,8 +22,8 @@ dotenv.config();
 initializeMongoDB();
 initializePassport();
 
-const allowedOrigins = process.env.CORS_ACCESS;
-const oauthCallbackUrls = process.env.CORS_ACCESS_OAUTH;
+const allowedOrigins = JSON.parse(process.env.CORS_ACCESS || '[]');
+const oauthCallbackUrls = JSON.parse(process.env.CORS_ACCESS_OAUTH || '[]');
 
 const corsOptions: cors.CorsOptions = {
     origin: function (origin, callback) {
